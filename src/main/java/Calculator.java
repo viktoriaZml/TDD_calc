29a9bec3311ab2da0e6a537965b37a360a18063d
vzemlyanovskaya <viktorry63@yandex.ru>
2021-04-25 23:25:28 +0700

    Calculator commit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,13 +1,28 @@
 public class Calculator {
   public double monthPayment(double sum, double rate, int monthNum) {
-    return 0;
+    /* Расчет месячного платежа
+    A = S * (r + r / (((1+r)^n)-1))
+    , A – ежемесячный аннуитетный платеж
+      S – сумма кредита
+      r – месячная процентная ставка по кредиту (годовая ставка деленная на 12 месяцев)
+      n – количество периодов (месяцев), в течение которых выплачивается кредит
+    */
+    double r = rate / 12 / 100;
+    return round(sum * (r + r / (Math.pow((1 + r), monthNum) - 1)));
   }
 
+  // Расчет общей суммы к возврату в банк
   public double allSum(double sum, double rate, int monthNum) {
-    return 0;
+    return round(monthPayment(sum, rate, monthNum) * monthNum);
   }
 
+  // Расчет переплаты за весь период
   public double overpayment(double sum, double rate, int monthNum) {
-    return 0;
+    return round(allSum(sum, rate, monthNum) - sum);
+  }
+
+  // Округление до 2х знаков
+  public double round(double d) {
+    return Math.ceil(d * 100) / 100;
   }
 }
